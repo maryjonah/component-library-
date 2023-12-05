498c0236c9855b6ccb07a4238668bade3c1ff684
Mary Jonah <jonahmary17@gmail.com>
2023-12-05 11:29:28 -0800

    Updated title font for cards component

--- demo/CardDemo.jsx
+++ demo/CardDemo.jsx
@@ -4,7 +4,7 @@ import Cards from "../components/Cards/Cards.jsx";
 export default function CardDemo() {
     return (
         <section className="component">
-            <h3 className="component-title">CARDS</h3>
+            <h3 className="component--title">CARDS</h3>
             <Cards 
                 title="Easy Deployment" 
                 description="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis." 
